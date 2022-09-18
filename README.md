f48c01388b7dd6816b1248906d60d31aad6adafb
DevJhonatanUD <jdmorenob@correo.udistrital.edu.co>
2022-09-17 21:10:34 -0500

    inicio

---
+++ README.md
@@ -0,0 +1,13 @@
+# Java desde 0
+## Tabla de contenido 
+- [Primeros Pasos](#primerospasos).
+
+### PrimerosPasos
+#### Variables
+	- Variables Primitivas
+	- Operadores Aritméticos
+	- Operadores de aignación
+	- Operadores de Comparación
+	- Operadores lógicos
+	El código desarrollado en la clase: 
+[Clase 1](https://github.com/DevJhonatanUD/JavaDesdeCero/blob/main/src/clasesReconocimiento/Variables.java)
--- src/clasesReconocimiento/Variables.java
+++ src/clasesReconocimiento/Variables.java
@@ -3,9 +3,72 @@ package clasesReconocimiento;
 public class Variables {
 
 	public static void main(String[] args) {
+
+		/*
+		 * Tipos de datos primitivos
+		 */
+		
+		// String
+		String cadenaDeCaracteres = "Hola", x2 = "Mundo";
+		System.out.println(cadenaDeCaracteres + " " + x2); //Concatenación de cadenas de carácteres
+
+		// Int
+		int numero1;
+		numero1 = 9;
+		System.out.println(numero1);
+
+		// Double
+		double double1 = 10 / 3;
+		System.out.println(double1);
+
+		// Float
+		float float1 = 10.3f, xx2;
+		System.out.println(float1);
+
+		// Char
+		char caracter = 's';
+		System.out.println(caracter);
+
+		// Booleano
+		boolean booleano1 = true;
+		System.out.println(booleano1);
 		
 		
-		
-	}
 	
+		/*
+		 * Operadores Aritméticos
+		 */
+		
+		/*
+		 + → Adición
+		 - → Sustracción
+		 * → Multiplicación
+		 / → Divisón
+		 % → Modulo
+		 ++ → Incremento
+		 -- → Decremento	 
+		 */
+
+		
+		
+		/*
+		 * Operadores de Asignación
+		 */
+		
+		/*
+		 =
+		 +=
+		 -=
+		 *=
+		 /=
+		 %=
+		 */
+		
+		/*
+		 * Operadores de comparación
+		 */
+		
+
+	}
+
 }
