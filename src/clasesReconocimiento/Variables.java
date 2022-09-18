617b0fe967d1ad6fbcd88c201e3f5aae1a197378
DevJhonatanUD <jdmorenob@correo.udistrital.edu.co>
2022-09-17 20:46:23 -0500

    ec

--- src/clasesReconocimiento/Variables.java
+++ src/clasesReconocimiento/Variables.java
@@ -2,4 +2,10 @@ package clasesReconocimiento;
 
 public class Variables {
 
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
 }
---
+++ src/estructurasControl/IfElse.java
@@ -0,0 +1,16 @@
+package estructurasControl;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		
+		ejercicio1();
+	}
+	
+	
+	public static void ejercicio1() {
+		
+		System.out.println("Hola");
+	}
+	
+}
